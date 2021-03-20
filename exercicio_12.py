eced509e9da2abfec614aed55c9f567920d61634
vinicius-blkIT <vsantana.nm@outlook.com>
2021-03-20 11:49:38 -0300

    corrigindo a nomenclatura e refazendo o exercício 12

---
+++ exercicio_12.py
@@ -0,0 +1,17 @@
+print("###################################################")
+print("  cálculo de peso ideal - para homens e mulheres")
+print("###################################################")
+
+nome = input("insira o seu nome para iniciarmos: ")
+print("olá {}, tudo bem? vamos prosseguir.".format(nome))
+
+print("___________________________________________________")
+
+altura = float(input("agora insira a sua altura em metros para realizarmos o cálculo: "))
+
+print("___________________________________________________")
+
+peso_ideal = (72.7 * altura) - 58
+print ("{}, com base na sua altura, seu peso ideal é {:.1f} kg!".format(nome,peso_ideal))
+
+print("fim do programa")
